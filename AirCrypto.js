79fb99766a6668f480dd70b18babd06558e1b453
nicolazaru <41578325+nicolazaru@users.noreply.github.com>
2018-07-27 10:43:16 +0200

    Add files via upload

--- AirCrypto.js
+++ AirCrypto.js
@@ -9,7 +9,7 @@ exports.getRateNow = function (MID,APIkey){
     var request = require('request-promise');
     var dateFormat = require('dateformat');
     var cryptoJS = require ('crypto-js');
-    var header = require ('./generatePBXHeader.js');
+    var header = require ('./headergen.js');
     const rn = require ('./randomno.js');
     var nonce = rn.randomnumber(5);
     var now = new Date();
@@ -18,7 +18,7 @@ exports.getRateNow = function (MID,APIkey){
     var URI = "https://previewapi.paidbycoins.com/v1/cli/rates";
     var payload='';
 
-    var signature = header.genHeader(MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
+    var signature = header.headergen(MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
     return request({
             "uri": URI,
             "method": httpmethod,
@@ -48,7 +48,7 @@ exports.createPayment = function (CryptoCurrency,Currency,Amount,MerchantRefNumb
     var querystring = require('querystring');
     var dateFormat = require('dateformat');
     var cryptoJS = require ('crypto-js');
-    var header = require ('./headergen_cp.js');
+    var header = require ('./headergen.js');
     const rn = require ('./randomno.js');
     var nonce = rn.randomnumber(5);
     var now = new Date();
@@ -88,4 +88,45 @@ exports.createPayment = function (CryptoCurrency,Currency,Amount,MerchantRefNumb
                 return response;
             });
 }
-       
+
+exports.paymentStatus = function (MID,APIkey,paymentID) {
+    var Promise = require("bluebird");
+    var crypto = require("crypto");
+    var randomNumber = require("random-number-csprng");
+    var base64 = require('base-64');
+    var request = require('request-promise');
+    var querystring = require('querystring');
+    var dateFormat = require('dateformat');
+    var cryptoJS = require ('crypto-js');
+    var header = require ('./generatePBXHeader.js');
+    const rn = require ('./randomno.js');
+    var nonce = rn.randomnumber(5);
+    var now = new Date();
+    var timestamp = dateFormat(now, "yyyymmdd hh:MM:ss");
+    var httpmethod="GET";
+    var URI = "https://previewapi.paidbycoins.com/v1/cli/status/" + paymentID;
+
+    var payload = "";
+
+    var signature = header.genHeader(MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
+    return request({
+            "encoding": 'utf8',
+            "uri": URI,
+            "method": httpmethod,
+            "apikey": APIkey,
+            "JSON" : true,
+            "body": payload,
+            "headers":{
+                "content-type":"application/json",
+                "pbcx": signature,
+                "User-Agent" : "Request"
+            }},
+        function(error, response, body) {
+            if(error) {
+                return console.error('couldnt connect', error);
+            }
+        })
+        .then(function(response) {
+            return response;
+        });
+}
