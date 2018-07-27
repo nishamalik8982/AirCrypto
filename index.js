27b8c5f8cc66dc6f22a3bc6767ba63d4fef3dcb9
nicolazaru <41578325+nicolazaru@users.noreply.github.com>
2018-07-27 18:31:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,139 @@
-
 'use strict'
 
 
-var aircrypto = require ('./AirCrypto.js')
+exports.getQuote = function (amount, fiatCurrency, cryptoType){
+    var Promise = require("bluebird");
+    var crypto = require("crypto");
+    var randomNumber = require("random-number-csprng");
+    var base64 = require('base-64');
+    var request = require('request-promise');
+    var dateFormat = require('dateformat');
+    var cryptoJS = require ('crypto-js');
+    var header = require ('./headergen.js');
+    const rn = require ('./randomno.js');
+    var nonce = rn.randomnumber(5);
+    var now = new Date();
+    var timestamp = dateFormat(now, "yyyymmdd hh:MM:ss");
+    var httpmethod="GET";
+    var MID = "CFF7D059-5EFC-49B2-BD18-B937261943B5";
+    var APIkey ="b25c9ae6f91d4c9f948b67ec00113456";
+    var URI = "https://previewapi.paidbycoins.com/v1/cli/rates";
+    var payload='';
 
-var paymentID = "";
-// Quantities to get from user
-var amount = 234.0;
-var fiatCurrency = "AUD"; // or USD ?
-var cryptoType = "XRP";
-var userEmail = "user@gmail.com";
-var MerchantRefNumber = "345345345"; // ??
+    var signature = header.headergen(MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
+    return request({
+            "uri": URI,
+            "method": httpmethod,
+            "apikey": APIkey,
+            "JSON" : true,
+            "headers":{
+                "pbcx": signature,
+                "User-Agent" : "Request-Promise"
+            }},
+        function(error, response, body) {
+            if (error) {
+                //handle error
+                console.log(error);
+            }
+        })
+        .then(function(response) {
+            return response;
+        });
+}
 
-aircrypto.getQuote(amount, fiatCurrency, cryptoType).then(function (res) {
-    let quotes = JSON.parse(res);
-    let y = quotes.Data.filter(function(element){
-        return element.PrimaryCurrency === cryptoType && element.SecondaryCurrency === fiatCurrency;
+exports.createPayment = function (userEmail, cryptoType, Amount,MerchantRefNumber){
+    var Promise = require("bluebird");
+    var crypto = require("crypto");
+    var randomNumber = require("random-number-csprng");
+    var base64 = require('base-64');
+    var request = require('request-promise');
+    var querystring = require('querystring');
+    var dateFormat = require('dateformat');
+    var cryptoJS = require ('crypto-js');
+    var header = require ('./headergen.js');
+    const rn = require ('./randomno.js');
+    var nonce = rn.randomnumber(5);
+    var now = new Date();
+    var timestamp = dateFormat(now, "yyyymmdd hh:MM:ss");
+    var MID = "CFF7D059-5EFC-49B2-BD18-B937261943B5";
+    var APIkey ="b25c9ae6f91d4c9f948b67ec00113456";
+    var httpmethod="POST";
+    var Currency = "AUD";
+    var URI = "https://previewapi.paidbycoins.com/v1/cli/createpayment";
+
+    var payload = JSON.stringify({
+        CryptoCurrency: cryptoType,
+        Currency: Currency,
+        Amount: Amount,
+        Detail:{
+            MerchantRefNo: MerchantRefNumber,
+            Email: userEmail
+        }
     });
-    console.log('QUOTE: %f %s == %d %s',amount.toFixed(2),fiatCurrency,(amount/y[0].Price).toFixed(2),cryptoType);
-});
 
+    var signature = header.headergen (MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
+    return request.post({
+            "encoding": 'utf8',
+            "uri": URI,
+            "method": httpmethod,
+            "apikey": APIkey,
+            "JSON" : true,
+            "body": payload,
+            "headers":{
+                "content-type":"application/json",
+                "pbcx": signature,
+                "User-Agent" : "Request"
+            }},
+        function(error, response, body) {
+            if(error) {
+                return console.error('couldnt connect', error);
+            }
+        })
+        .then(function(response) {
+            return response;
+        });
+}
 
+exports.paymentStatus = function (paymentID) {
+    var Promise = require("bluebird");
+    var crypto = require("crypto");
+    var randomNumber = require("random-number-csprng");
+    var base64 = require('base-64');
+    var request = require('request-promise');
+    var querystring = require('querystring');
+    var dateFormat = require('dateformat');
+    var cryptoJS = require ('crypto-js');
+    var header = require ('./headergen.js');
+    const rn = require ('./randomno.js');
+    var nonce = rn.randomnumber(5);
+    var now = new Date();
+    var timestamp = dateFormat(now, "yyyymmdd hh:MM:ss");
+    var MID = "CFF7D059-5EFC-49B2-BD18-B937261943B5";
+    var APIkey ="b25c9ae6f91d4c9f948b67ec00113456";
+    var httpmethod="GET";
+    var URI = "https://previewapi.paidbycoins.com/v1/cli/status/" + paymentID;
 
-aircrypto.createPayment(userEmail, cryptoType, amount,MerchantRefNumber).then(function (res) {
-    let payment = JSON.parse(res);
-    paymentID = payment.Data.PaymentID;
+    var payload = "";
 
-    console.log('PAYMENT: status: %s, payment id: %s', payment.Status, paymentID);
-
-    aircrypto.paymentStatus(paymentID).then(function (res) {
-        let paymentStatus = JSON.parse(res);
-        console.log('PAYMENT STATUS: %s,  %s', payment.Status, paymentStatus.Data.Status);
-    });
-});
+    var signature = header.headergen(MID, httpmethod,URI, timestamp, nonce, payload, APIkey);
+    return request({
+            "encoding": 'utf8',
+            "uri": URI,
+            "method": httpmethod,
+            "apikey": APIkey,
+            "JSON" : true,
+            "body": payload,
+            "headers":{
+                "content-type":"application/json",
+                "pbcx": signature,
+                "User-Agent" : "Request"
+            }},
+        function(error, response, body) {
+            if(error) {
+                return console.error('couldnt connect', error);
+            }
+        })
+        .then(function(response) {
+            return response;
+        });
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+
+'use strict'
+
+
+var aircrypto = require ('./AirCrypto.js')
+
+var paymentID = "";
+// Quantities to get from user
+var amount = 234.0;
+var fiatCurrency = "AUD"; // or USD ?
+var cryptoType = "XRP";
+var userEmail = "user@gmail.com";
+var MerchantRefNumber = "345345345"; // ??
+
+aircrypto.getQuote(amount, fiatCurrency, cryptoType).then(function (res) {
+    let quotes = JSON.parse(res);
+    let y = quotes.Data.filter(function(element){
+        return element.PrimaryCurrency === cryptoType && element.SecondaryCurrency === fiatCurrency;
+    });
+    console.log('QUOTE: %f %s == %d %s',amount.toFixed(2),fiatCurrency,(amount/y[0].Price).toFixed(2),cryptoType);
+});
+
+
+
+aircrypto.createPayment(userEmail, cryptoType, amount,MerchantRefNumber).then(function (res) {
+    let payment = JSON.parse(res);
+    paymentID = payment.Data.PaymentID;
+
+    console.log('PAYMENT: status: %s, payment id: %s', payment.Status, paymentID);
+
+    aircrypto.paymentStatus(paymentID).then(function (res) {
+        let paymentStatus = JSON.parse(res);
+        console.log('PAYMENT STATUS: %s,  %s', payment.Status, paymentStatus.Data.Status);
+    });
+});
