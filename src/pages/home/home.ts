6c639794e6ea3292a46dd2e9d9c38b1dc8f9c9a3
nicolazaru <41578325+nicolazaru@users.noreply.github.com>
2018-08-04 13:27:45 +0200

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Alert, NavController, NavParams} from 'ionic-angular';
 import { getQuote, createPayment, paymentStatus } from '@nishamalik8982/aircrypto';
 import { HttpClient,HttpErrorResponse } from '@angular/common/http';
+import { Select } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/forms";
 
@@ -9,29 +11,45 @@ import {FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/form
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   /*formgroup:FormGroup;
   amount:AbstractControl;
   cryptoType:AbstractControl;*/
-  public amount:number = null;
+  public secondaryCurrencyOptions:Array<string>;
+
+
+  @ViewChild('selectCrypto') selectCrypto: Select;
   public cryptoType:string = "";
+
+
+  // Currencies
+  public primaryCurrency:string = "";
+  public secondaryCurrency:string = "";
+  public Crypto:string = "";
+  public secondaryCurrencyIsFiat:boolean = false;
+  public amount:number = null;
+  public method:string = "";
+
   public fiatcurrency:string = "";
   public email:string = "";
   public quotedAmount:number = null;
   public hideShowCrypto: boolean = false;
+  public showSecondaryCurrency: boolean = false;
+  public hideShowFiat: boolean = false;
+  public inputAmount: boolean = false;
   public confirmQuote: boolean = false;
   public status: boolean = false;
-  public payload:any;
   private Payment:any = {
     ID: null,
     address: "",
     cryptoAmount: null
   };
 
-  public MerchantRefNumber:string = "345345345";
 
 
-  constructor(public navCtrl: NavController, public http: HttpClient
+  constructor(public navCtrl: NavController,
+              public http: HttpClient
               /*public  navParams: NavParams,
               public formBuilder: FormBuilder*/) {
                   /*this.formgroup = formBuilder.group({
@@ -42,22 +60,50 @@ export class HomePage {
                   this.cryptoType= this.formgroup.controls['cryptoType'];*/
 
   }
-  public showHideCurrency() {
-    this.hideShowCrypto = true;
+
+  public getPrimaryCurrency() {
+    if (this.primaryCurrency === "CRYPTO") {
+      this.selectCrypto.open(); // select which crypto
+      this.secondaryCurrencyIsFiat = true;
+    } else if (this.primaryCurrency === "AUD" || this.primaryCurrency === "USD") {
+      this.secondaryCurrencyIsFiat = false;
+    }
+    this.getSecondaryCurrency();
+  }
+  public updatePrimaryCurrency() {
+    this.primaryCurrency = this.Crypto; // pre-set the cryptocurrency as primary currency
+  }
+
+
+  public getSecondaryCurrency() {
+    if ( this.secondaryCurrencyIsFiat ) {
+      this.secondaryCurrencyOptions = ["USD", "AUD"];
+    } else {
+      this.secondaryCurrencyOptions = ["ETH", "BTC", "LTC", "BCH"];
+    }
+    this.showSecondaryCurrency = true; // show options in the form
+  }
+
+  public showAmount() {
+    this.inputAmount = true;
   }
 
 
 
   public getRate() {
     let that = this;
-    console.log(this.amount, this.fiatcurrency, this.cryptoType);
+    console.log(this.amount, this.primaryCurrency, this.secondaryCurrency);
     document.getElementById('quoteResponse').textContent = 'Getting quote...';
     that.confirmQuote = true;
-    console.log("button on");
-    getQuote(this.amount, this.fiatcurrency, this.cryptoType).then(function (res) {
-      document.getElementById('quoteResponse').textContent = 'Your quote: ' + that.amount + ' ' + that.fiatcurrency.toString() + ' = ' + res + ' ' + that.cryptoType;
-      that.quotedAmount = res;
-      that.status = true;
+    getQuote(this.amount, this.primaryCurrency, this.secondaryCurrency).then(function (res) {
+      document.getElementById('quoteResponse').textContent = 'Your quote: ' + that.amount + ' ' + that.primaryCurrency.toString() + ' = ' + res + ' ' + that.secondaryCurrency;
+      if (that.secondaryCurrencyIsFiat && res < 30) {
+        alert("Minimum amount is:  " + (that.amount*30/res).toFixed(4).toString() + that.primaryCurrency );
+
+      } else {
+        that.quotedAmount = res;
+        that.status = true;
+      }
     }).catch(function (e) {
       alert(e.message);
     });
@@ -65,31 +111,39 @@ export class HomePage {
 
   public createPayment() {
     let that = this;
-    createPayment(this.email, this.cryptoType, this.amount, this.MerchantRefNumber).then(function (res) {
-      let response = JSON.parse(res);
-      that.Payment = {
-        ID: response.Data.PaymentID,
-        address: response.Data.CryptoAddress,
-        cryptoAmount: response.Data.CryptoAmount
-      };
-      alert(
-        "\npaymentID:   " + that.Payment.ID +
-        "\ncryptoAddress:   " + that.Payment.address +
-        "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.cryptoType);
+    if (this.secondaryCurrencyIsFiat) {
+      createPayment(this.email, this.primaryCurrency, this.quotedAmount).then(function (res) {
+        console.log(that.email, that.primaryCurrency, that.quotedAmount);
+        let response = JSON.parse(res);
+        that.Payment = {
+          ID: response.Data.PaymentID,
+          address: response.Data.CryptoAddress,
+          cryptoAmount: response.Data.CryptoAmount
+        };
+        alert(
+          "\npaymentID:   " + that.Payment.ID +
+          "\ncryptoAddress:   " + that.Payment.address +
+          "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.primaryCurrency);
+      }).catch(function (e) {
+        alert(e.message);
+      });
+    } else {
+      createPayment(this.email, this.secondaryCurrency, this.amount).then(function (res) {
+        let response = JSON.parse(res);
+        that.Payment = {
+          ID: response.Data.PaymentID,
+          address: response.Data.CryptoAddress,
+          cryptoAmount: response.Data.CryptoAmount
+        };
+        alert(
+          "\npaymentID:   " + that.Payment.ID +
+          "\ncryptoAddress:   " + that.Payment.address +
+          "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.secondaryCurrency);
+      }).catch(function (e) {
+        alert(e.message);
+      });
+    }
 
-      /*if (that.quotedAmount === that.Payment.cryptoAmount) {
-        alert("Details of your payment:" +
-          "paymentID: " + that.Payment.ID +
-          "cryptoAddress: " + that.Payment.address +
-          "cryptoAmount: " + that.Payment.cryptoAmount);
-      } else {
-        alert("Quote matching error");
-        console.log(that.quotedAmount, that.Payment.cryptoAmount);
-      }*/
-
-    }).catch(function (e) {
-      alert(e.message);
-    });
   }
   /*public createPayment() {
     console.log('Creating payment');
