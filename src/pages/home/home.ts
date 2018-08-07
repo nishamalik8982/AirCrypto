2ca94f66fc39626a72d73700ff010e8a4bb7dac1
nicolazaru <41578325+nicolazaru@users.noreply.github.com>
2018-08-07 15:21:11 +0200

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,19 +13,23 @@ import {FormBuilder, FormGroup, Validators, AbstractControl} from "@angular/form
 })
 
 export class HomePage {
-  /*formgroup:FormGroup;
-  amount:AbstractControl;
-  cryptoType:AbstractControl;*/
+
+  // List of cryptocurrencies
   public  cryptoCurrency:Array<string> = ["ETH", "BTC", "LTC", "BCH"];
+
+  // To
+  public type:string = ""; // FIAT or CRYPTO
+
+  // To display/hide input field
   public showCrypto:boolean = false;
   public showFiat:boolean = false;
-  public type:string = ""; // FIAT or CRYPTO
+
+
+
 
 
 
   @ViewChild('selectCrypto') selectCrypto: Select;
-  public cryptoType:string = "";
-
 
   // Currencies
   public primaryCurrency:string = "";
@@ -33,19 +37,17 @@ export class HomePage {
   public secondaryCurrencyIsFiat:boolean = false;
   public amount:number = null;
   public quoteResponse:boolean = false;
-
+  public status: boolean = false;
   public email:string = "";
   public quotedAmount:number = null;
   public inputAmount: boolean = false;
   public confirmQuote: boolean = false;
-  public status: boolean = false;
+
   private Payment:any = {
     ID: null,
     address: "",
     cryptoAmount: null
   };
-
-
 
   constructor(public navCtrl: NavController,
               public http: HttpClient)
@@ -78,74 +80,100 @@ export class HomePage {
     this.inputAmount = true;
   }
 
+  public showButtons() {
+    this.status = true;
+    this.confirmQuote = true;
+  }
 
+  public amountIsInRange() {
+    if (this.secondaryCurrencyIsFiat ) {
+      if (this.quotedAmount < 30){
+        alert("Minimum amount is:  " + (this.amount*30/this.quotedAmount).toFixed(4).toString() + this.primaryCurrency );
+        return false;
+      } else if(this.quotedAmount > 1000) {
+        alert("Maximum amount is:  " + (this.amount*1000/this.quotedAmount).toFixed(4).toString() + this.primaryCurrency );
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      if (this.amount < 30){
+        alert("Minimum amount is:  30 AUD" );
+        return false;
+      } else if(this.amount > 1000) {
+        alert("Maximum amount is:  1000 AUD" );
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
 
   public getRate() {
     let that = this;
     console.log(this.amount, this.primaryCurrency, this.secondaryCurrency);
     document.getElementById('quoteResponse').textContent = 'Getting quote...';
-    that.confirmQuote = true;
+    this.showButtons();
     getQuote(this.amount, this.primaryCurrency, this.secondaryCurrency).then(function (res) {
       document.getElementById('quoteResponse').textContent = 'Your quote: ' + that.amount + ' ' + that.primaryCurrency.toString() + ' = ' + res + ' ' + that.secondaryCurrency;
-      if (that.secondaryCurrencyIsFiat && res < 30) {
-        alert("Minimum amount is:  " + (that.amount*30/res).toFixed(4).toString() + that.primaryCurrency );
-
-      } else {
-        that.quotedAmount = res;
-        that.status = true;
-      }
+      that.quotedAmount = res;
     }).catch(function (e) {
       alert(e.message);
     });
   }
 
-  public createPayment() {
-    let that = this;
-    if (this.secondaryCurrencyIsFiat) {
-      createPayment(this.email, this.primaryCurrency, this.quotedAmount).then(function (res) {
-        console.log(that.email, that.primaryCurrency, that.quotedAmount);
-        let response = JSON.parse(res);
-        that.Payment = {
-          ID: response.Data.PaymentID,
-          address: response.Data.CryptoAddress,
-          cryptoAmount: response.Data.CryptoAmount
-        };
-        alert(
-          "\npaymentID:   " + that.Payment.ID +
-          "\ncryptoAddress:   " + that.Payment.address +
-          "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.primaryCurrency);
-      }).catch(function (e) {
-        alert(e.message);
-      });
-    } else {
-      createPayment(this.email, this.secondaryCurrency, this.amount).then(function (res) {
-        let response = JSON.parse(res);
-        that.Payment = {
-          ID: response.Data.PaymentID,
-          address: response.Data.CryptoAddress,
-          cryptoAmount: response.Data.CryptoAmount
-        };
-        alert(
-          "\npaymentID:   " + that.Payment.ID +
-          "\ncryptoAddress:   " + that.Payment.address +
-          "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.secondaryCurrency);
-      }).catch(function (e) {
-        alert(e.message);
-      });
-    }
 
+  public createPayment() {
+    // If    30 < AUD < 1000
+    if (this.amountIsInRange()) {
+      let that = this;
+      if (this.secondaryCurrencyIsFiat) {
+        createPayment(this.email, this.primaryCurrency, this.quotedAmount).then(function (res) {
+          console.log(that.email, that.primaryCurrency, that.quotedAmount);
+          let response = JSON.parse(res);
+          that.Payment = {
+            ID: response.Data.PaymentID,
+            address: response.Data.CryptoAddress,
+            cryptoAmount: response.Data.CryptoAmount
+          };
+          alert(
+            "\npaymentID:   " + that.Payment.ID +
+            "\ncryptoAddress:   " + that.Payment.address +
+            "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.primaryCurrency);
+        }).catch(function (e) {
+          alert(e.message);
+        });
+      } else {
+        createPayment(this.email, this.secondaryCurrency, this.amount).then(function (res) {
+          let response = JSON.parse(res);
+          that.Payment = {
+            ID: response.Data.PaymentID,
+            address: response.Data.CryptoAddress,
+            cryptoAmount: response.Data.CryptoAmount
+          };
+          alert(
+            "\npaymentID:   " + that.Payment.ID +
+            "\ncryptoAddress:   " + that.Payment.address +
+            "\ncryptoAmount:   " + that.Payment.cryptoAmount + "  " + that.secondaryCurrency);
+        }).catch(function (e) {
+          alert(e.message);
+        });
+      }
+    } else return;
   }
 
 
   public paymentStatus() {
-    let that = this;
-    paymentStatus(that.Payment.ID).then(function (res) {
-      let response = JSON.parse(res);
-      let TransactionStatus = response.Data.Status;
-      alert("\nPayment Status:   " + TransactionStatus);
-    }).catch(function (e) {
-      alert(e.message);
-    });
+    if (this.amountIsInRange()) {
+      let that = this;
+      paymentStatus(that.Payment.ID).then(function (res) {
+        let response = JSON.parse(res);
+        let TransactionStatus = response.Data.Status;
+        alert("\nPayment Status:   " + TransactionStatus);
+      }).catch(function (e) {
+        alert(e.message);
+      });
+    } else return;
   }
 
 
